7bf83f74bbce765d15da026e7d31610b5bdc0052
Reynaldo Esparza <reynaldo@esporaestudio.com>
2016-05-30 16:12:59 -0500

    Configurar rutas

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,10 +11,7 @@
 |
 */
 
-//Route::get('/', 'WelcomeController@index');
-Route::get('/', function(){
-	return view('auth.register');
-});
+Route::get('/', 'WelcomeController@index');
 
 Route::get('home', 'HomeController@index');
 
@@ -22,3 +19,76 @@ Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
 ]);
+
+
+/*
+|--------------------------------------------------------------------------
+| Modifying the Default Authentication Routes
+|--------------------------------------------------------------------------
+|
+|
+*/
+// Example 1
+// login url http://www.example.com/account/login
+// logout url http://www.example.com/account/logout
+// registration url http://www.example.com/account/register
+/*Route::controllers([
+    'account' => 'Auth\AuthController',
+    'password' => 'Auth\PasswordController',
+]);*/
+
+// Example 2
+// login url http://www.example.com/login
+// logout url http://www.example.com/logout
+// registration url http://www.example.com/register
+/*Route::controllers([
+    '' => 'Auth\AuthController',
+    'password' => 'Auth\PasswordController',
+]);*/
+
+// Example 3
+// redefine all routes
+/*Route::get('example/register', 'Auth\AuthController@getRegister');
+Route::post('example/register', 'Auth\AuthController@postRegister');
+Route::get('example/login', 'Auth\AuthController@getLogin');
+Route::post('example/login', 'Auth\AuthController@postLogin');
+Route::get('example/logout', 'Auth\AuthController@getLogout');
+Route::get('example/email', 'Auth\PasswordController@getEmail');
+Route::post('example/email', 'Auth\PasswordController@postEmail');
+Route::get('example/reset/{code}', 'Auth\PasswordController@getReset');
+Route::post('example/reset', 'Auth\PasswordController@postReset');*/
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Securing Your Routes
+|--------------------------------------------------------------------------
+|
+| Here are a few examples of how to use it to protect your routes
+|
+*/
+// route closure
+/*Route::get('<your-route-uri>', ['middleware' => 'auth', function()
+{
+    // if user is not logged in
+    // he/she will be redirected to the login page
+    // and this code will not be executed
+}]);
+
+// controller action
+Route::get('<your-route-uri>', ['middleware' => 'auth', 'uses' => '<your-controller>@<your-action>']);
+
+// within a controller
+class YourController extends Controller {
+
+    public function __construct()
+    {
+        $this->middleware('<your-middleware-name>');
+
+        $this->middleware('<another-middleware>', ['only' => ['<some-action-name>']]);
+
+        $this->middleware('<more-middleware>', ['except' => ['<another-action-name>']]);
+    }
+
+}*/
